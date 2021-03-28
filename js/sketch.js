aa4b7a33b36243c94c0c95b1ebec57874a648583
xtremelibasic <xtremeli.basic@gmail.com>
2021-03-28 16:51:55 +0100

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+// let page = document.getElementById("page-wrapper");
+// let main = document.querySelector("main");
+// main.innerHTML = page + main.innerHTML;
+// function setup() {
+//     createCanvas(400, 400);
+//   }
+  
+//   function draw() {
+//     background(220);
+//     ellipse(50,50,80,80);
+//   }
